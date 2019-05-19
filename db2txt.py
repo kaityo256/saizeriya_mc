5cad6fb732a44486cf8896c13ca23153c3bdff1d
H. Watanabe <kaityo@users.sourceforge.jp>
2019-05-19 23:17:42 +0900

    adds README

--- db2txt.py
+++ db2txt.py
@@ -2,7 +2,12 @@ import sqlite3
 
 conn = sqlite3.connect('saizeriya.db')
 c = conn.cursor()
-for (p, n, c) in c.execute('SELECT name, price, calorie FROM menu'):
+fn = open("name.txt","w")
+fp = open("price.txt","w")
+fc = open("calorie.txt","w")
+for (n, p, c) in c.execute('SELECT name, price, calorie FROM menu'):
+    fp.write(str(p) + "\n")
+    fn.write(str(n) + "\n")
+    fc.write(str(c) + "\n")
     print(p,n,c)
 conn.close()
-
