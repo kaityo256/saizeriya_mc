2ac4fef263138623bb40ea7fc8123405e06bbd31
H. Watanabe <kaityo@users.sourceforge.jp>
2019-06-07 11:15:50 +0900

    revises pickle

--- db2pickle.py
+++ db2pickle.py
@@ -1,15 +1,16 @@
 import pickle
 import sqlite3
 
-conn = sqlite3.connect('saizeriya.db')
 names = []
 prices = []
 cals = []
 
-for (n, p, c) in conn.cursor().execute('SELECT name, price, calorie FROM menu'):
-    names.append(n)
-    prices.append(p)
-    cals.append(c)
+with sqlite3.connect('saizeriya.db') as conn:
+    cur = conn.cursor()
+    for (n, p, c) in cur.execute('SELECT name, price, calorie FROM menu'):
+        names.append(n)
+        prices.append(p)
+        cals.append(c)
 
 with open('saizeriya.pickle', 'wb') as f:
     pickle.dump((names, prices, cals), f)
--- read.py
+++ read.py
@@ -3,4 +3,5 @@ import pickle
 with open('saizeriya.pickle', 'rb') as f:
     names, prices, cals = pickle.load(f)
 
+print(names)
 
